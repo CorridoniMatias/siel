4c564f1c2c8f4d6df03e6a890e1a90369f893c8a
Matias MC <matim97@gmail.com>
2018-11-07 16:45:24 -0300

    Normas de vectores y matrices listo

--- dependencias/PE.js
+++ dependencias/PE.js
@@ -1,15 +1,28 @@
 $(document).ready(function () {
 
+	console.log(normas.matrices.norma1());
 
-	
+    console.log(normas.matrices.normaInfinito());
 
+    console.log(normas.matrices.norma2());
 
+    let vec = [
+    	[1],
+		[-20],
+		[3]
+	];
+
+    console.log(normas.vectores.normap(vec, 3));
 });
 
 //Funciones
 
 var vectorInicial = [];
-var matrixCoeficientes = [][];
+var matrixCoeficientes = [
+	[1,2,3],
+	[1,5,3],
+	[5,9,2]
+];
 
 var normas = 
 {
@@ -18,14 +31,51 @@ var normas =
 		norma1: function()
 		{
 			//Sumar por cada columna en valor absoluto los valores y quedarse con el mayor de cada uno.
+			let tmp = [];
+
+			for(let i = 0; i < matrixCoeficientes.length;i++)
+				for(let j = 0; j < matrixCoeficientes[i].length;j++)
+				{
+					if(typeof tmp[j] === "undefined")
+						tmp[j] = 0;
+
+                    tmp[j] += Math.abs(matrixCoeficientes[i][j]);
+				}
+
+			return Math.max(...tmp);
 		},
 		normaInfinito: function()
 		{
-			//sumar por cada fila en valor absoluto los valores y quedarse con el mayor de cada uno 
+			//sumar por cada fila en valor absoluto los valores y quedarse con el mayor de cada uno
+            let tmp = [];
+
+            for(let i = 0; i < matrixCoeficientes.length;i++)
+                for(let j = 0; j < matrixCoeficientes[i].length;j++)
+                {
+                    if(typeof tmp[j] === "undefined")
+                        tmp[j] = 0;
+
+                    tmp[i] += Math.abs(matrixCoeficientes[i][j]);
+                }
+
+            return Math.max(...tmp);
 		},
 		norma2: function()
 		{
+
 			//la transpuesta de la matrix de coeficientes por la matrix de coeficientes y de ahi sacar el mayor en modulo de los autovalores y despues la raiz cuadrada del autovalor.
+
+            let transpuesta = math.transpose(matrixCoeficientes);
+
+            console.log(transpuesta);
+
+            let crosseada = math.multiply(transpuesta, matrixCoeficientes);
+
+            let autovalores = numeric.eig(crosseada).lambda.x;
+
+            autovalores.map(v => Math.abs(v));
+
+            return Math.sqrt( Math.max(...autovalores) );
 		}
 	},
 	vectores:
@@ -33,21 +83,51 @@ var normas =
 		norma2: function(vector)
 		{
 			// raiz cuadrada de la suma de los elementos del vector al cuadrado
+			return this.normap(vector, 2);
 		},
 		normaInfinito: function(vector)
 		{
 			// mayor de los modulos de los elementos
+
+            let mapped = vector.map(el => Math.abs(el));
+
+            return Math.max(...mapped);
 		},
 		normap(vector, p)
 		{
 			//raiz p-esima de la suma de los elementos del vector en modulo a la p
+
+            let newvector = vector.map(v => Math.abs(v)).map(v => Math.pow(v, p));
+
+            let suma = newvector.reduce((acumulado, valorActual) => acumulado + valorActual);
+
+            return math.nthRoot(suma, p);
 		}
 	}
-}
+};
 
 function validarMatrixDiagonalmenteDominante()
 {
 	//el modulo de cada elemento de la diagonal tiene que ser mayor o igual que la suma de los demas elementos de la misma fila sumados en modulo.
+
+    let tmp = 0, diagonal = 0;
+
+    for(let i = 0; i < matrixCoeficientes.length;i++)
+    {
+        for(let j = 0; j < matrixCoeficientes[i].length;j++)
+        {
+            if(i+1 != j)
+                tmp += Math.abs(matrixCoeficientes[i][j]);
+            else
+                diagonal = matrixCoeficientes[i][j];
+
+            console.log("[" + i + "][" + j + "] = " + matrixCoeficientes[i][j]);
+        }
+
+        if(diagonal < tmp){
+
+        }
+    }
 }
 
 function validarMatrixEstrictamenteDominante()
@@ -75,7 +155,7 @@ var metodos =
 		{
 		}
 	}
-}
+};
 
 function jacobi()
 {
